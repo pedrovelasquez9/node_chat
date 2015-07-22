5ebedd4950e58f038cbd7b326a88e2f44533c020
Pedro Plasencia <pjpv9011@gmail.com>
2015-07-22 12:16:14 -0430

    config de puerto e ip

--- app.js
+++ app.js
@@ -22,4 +22,7 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(8080);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080; 
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+server.listen(port, ip);
